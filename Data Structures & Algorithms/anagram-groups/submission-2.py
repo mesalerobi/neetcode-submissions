cfa7347f212c28772680aeeba94ccb71b70874eb
mesalerobi <mrobi1@umbc.edu>
2026-08-07 19:44:25 -0500

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        wrds = {} # Letter Counts -> [Word, Word, ...]
+        for wrd in strs: # Iterate through list of words
+            counts = [0] * 26 # Track letter frequency in word
+            for ch in wrd: # Iterate through word by character
+                counts[ord(ch) - ord('a')] += 1 # Increment frequency of letter
+            if tuple(counts) in wrds: # Append word to list if frequency exists
+                wrds[tuple(counts)].append(wrd)
+            else: # Or add frequency as a new key
+                wrds[tuple(counts)] = [wrd]
+        final = []
+        for keys in wrds: # Iterate through keys to create returned list
+            final.append(wrds[keys])
+        return final 
